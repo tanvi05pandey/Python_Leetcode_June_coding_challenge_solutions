2127d0279ed9a8ff2e92c60f540b132bdc0b4fb0
tanvi05pandey <tanvi05pandey@gmail.com>
2020-06-03 15:20:37 -0700

    Update day3_twoCitySchedCost.py

--- day3_twoCitySchedCost.py
+++ day3_twoCitySchedCost.py
@@ -17,41 +17,6 @@
 # The total minimum cost is 10 + 30 + 50 + 20 = 110 to have half the people interviewing in each city.
 
 
-# ---------SOLUTION EXPLANATION:-------------------#
-# The problem is to send n persons to city A
-# and n persons to city B with minimum cost.
-#
-# The idea is to send each person to city A.
-# costs = [[10,20],[30,200],[400,50],[30,20]]
-#
-# So, totalCost = 10 + 30 + 400 + 30 = 470
-#
-# Now, we need to send n persons to city B.
-# Which persons do we need to send city B?
-#
-# Here, we need to minimize the cost.
-# We have already paid money to go to city A.
-# So, Send the persons to city B who get more refund
-# so that our cost will be minimized.
-#
-# So, maintain refunds of each person
-# refund[i] = cost[i][1] - cost[i][0]
-#
-# So, refunds of each person
-# refund = [10, 170, -350, -10]
-#
-# Here, refund +ve means we need to pay
-# -ve means we will get refund.
-#
-# So, sort the refund array.
-#
-# refund = [-350, -10, 10, 170]
-#
-# Now, get refund for N persons,
-#     totalCost += 470 + -350 + -10 = 110
-#
-# So, minimum cost is 110
-
 
 def twoCitySchedCost(costs):
     firstCity = [i for i,j in costs]
